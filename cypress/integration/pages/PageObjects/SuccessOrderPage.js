0323f1a05f1f556b6549269a6081147ed5c4b4a7
kferko7 <kim.ferko@hotmail.com>
2022-09-23 14:16:12 +0200

    update order class

--- cypress/integration/pages/PageObjects/SuccessOrderPage.js
+++ cypress/integration/pages/PageObjects/SuccessOrderPage.js
@@ -15,30 +15,26 @@ export class SuccessOrderPage extends BasePage {
 
     }
 
-	//find locator by customized by data test
     addAddSauceLabsBackpackToCart() {
         cy.get("button[data-test='add-to-cart-sauce-labs-backpack']").should('be.visible').click()
         
     }
     
-	//find locator by Class
     navigateToCart(){
         cy.get('.shopping_cart_badge').click()
     }
 
-	//find its lenghth 
     assertItemQuantityIntoCart() {
     //cy.get('.cart_quantity').should('have.length', 1)
 	cy.get('.cart_quantity')
 	.its('length')
 	.should('eq', 1)
     }
-    
-    //find locator by id
+
     checkoutButton(){
 	cy.get('#checkout').click()
-}
-	//find locator by Id
+    }
+	
     fillFirstName(value) {
 		const field = cy.get("#first-name")
 		field.clear()
@@ -46,7 +42,6 @@ export class SuccessOrderPage extends BasePage {
 		return this
 	}
     
-	//find locator by CSS
     fillLastName(value) {
 		const field = cy.get("#last-name")
 		field.clear()
@@ -54,7 +49,6 @@ export class SuccessOrderPage extends BasePage {
 		return this
 	}
 
-    //find locator by customized name
     fillZipCode(value) {
 		const field = cy.get("[name='postalCode']")
 		field.clear()
@@ -62,13 +56,12 @@ export class SuccessOrderPage extends BasePage {
 		return this
 	}
 
-	//has price --find by css locator
 	assertPrice() {
 		cy.get('.inventory_item_price').contains('$29.99')
 		cy.get('.summary_total_label').contains('Total: $32.39')
 
 	}
-    //find locator by customized name
+   
     continueButton() {
 		cy.get("input[name=continue]")
 			.should('be.visible', {
@@ -77,12 +70,10 @@ export class SuccessOrderPage extends BasePage {
 			.click()
 	}
 
-	//find locator by Css
-    finishButton() {
+	finishButton() {
 		cy.get('button#finish').click()
 	}
 
-	//assert with text displayed order complete
 	expectedCheckoutComplete() {
 		cy.get("h2").contains('THANK YOU FOR YOUR ORDER', {
 			timeout: 10000,
